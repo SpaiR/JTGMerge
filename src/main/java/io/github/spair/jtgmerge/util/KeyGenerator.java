9559fda7694414d757489c69b15644f128e76b0c
SpaiR <klimbetoo@gmail.com>
2018-12-31 18:47:58 +0300

    new key generation algorithm

--- src/main/java/io/github/spair/jtgmerge/util/KeyGenerator.java
+++ src/main/java/io/github/spair/jtgmerge/util/KeyGenerator.java
@@ -2,47 +2,62 @@ package io.github.spair.jtgmerge.util;
 
 import io.github.spair.dmm.io.DmmData;
 import lombok.val;
+import lombok.var;
 
 public final class KeyGenerator {
 
-    private static final char[] VALID_KEY_ELEMENTS = {
+    private static final int LIMIT = 65530;
+    private static final int BASE = 52;
+
+    private static final char[] BASE_52_KEYS = {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
             'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
 
     private final DmmData dmmData;
-    private int keyGeneratorCurrentId;
 
     public KeyGenerator(final DmmData dmmData) {
         this.dmmData = dmmData;
-        keyGeneratorCurrentId = (int) Math.pow(VALID_KEY_ELEMENTS.length, dmmData.getKeyLength() - 1);
     }
 
     public String createKey() {
-        while (true) {
-            int localId = keyGeneratorCurrentId++;
-            val generatedKey = new StringBuilder();
+        var freeKeys = Math.min(LIMIT, (Math.pow(BASE, dmmData.getKeyLength()))) - dmmData.getKeys().size();
+        var num = 1;
 
-            while (localId >= VALID_KEY_ELEMENTS.length) {
-                int i = localId % VALID_KEY_ELEMENTS.length;
-                generatedKey.insert(0, VALID_KEY_ELEMENTS[i]);
-                localId -= i;
-                localId /= VALID_KEY_ELEMENTS.length;
-            }
+        while (freeKeys > 0) {
+            if (!dmmData.hasTileContentByKey(numToKey(num)))
+                break;
+            num++;
+            freeKeys--;
+        }
 
-            generatedKey.append(VALID_KEY_ELEMENTS[localId]);
+        if (num > LIMIT)
+            throw new IllegalStateException("Maximum available num values is 65530. Current num: " + num);
 
-            if (dmmData.hasTileContentByKey(generatedKey.toString())) {
-                continue;
-            }
+        return numToKey(num);
+    }
 
-            if (generatedKey.length() == dmmData.getKeyLength()) {
-                return generatedKey.toString();
-            } else {
-                System.out.println("ERROR: Generated key is outside of bounds");
-                System.exit(1);
+    private String numToKey(final int num) {
+        StringBuilder result = new StringBuilder();
+        int currentNum = num;
+
+        while (currentNum > 0) {
+            result.insert(0, BASE_52_KEYS[currentNum % BASE]);
+            currentNum = (int) Math.floor(((double) currentNum) / BASE);
+        }
+
+        val keyLength = dmmData.getKeyLength();
+        if (result.length() > keyLength)
+            throw new IllegalStateException("Key length cannot less than result length");
+
+        val lengthDiff = keyLength - result.length();
+        if (lengthDiff != 0) {
+            for (int i = 0; i < lengthDiff; i++) {
+                result.insert(0, BASE_52_KEYS[0]);
             }
         }
+
+        return result.toString();
     }
 }
